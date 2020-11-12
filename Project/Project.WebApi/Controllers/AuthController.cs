9422d46b1829caad3259bdb103ce19b74b4449e5
ChumakMike <36298538+ChumakMike@users.noreply.github.com>
2020-11-12 21:49:01 +0200

    API developing controllers

---
+++ Project/Project.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.WebApi.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase {
+
+        public AuthController() {
+
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model) {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] Object obj) {
+            return Ok();
+        }
+    }
+}
---
+++ Project/Project.WebApi/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.WebApi.Models {
+    public class LoginModel {
+
+        [Required(AllowEmptyStrings = false)]
+        public string UserName { get; set; }
+        
+        [Required(AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
