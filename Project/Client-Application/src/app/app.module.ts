d19a3b494a25317839e869933a9483ee60147569
ChumakMike <36298538+ChumakMike@users.noreply.github.com>
2020-11-21 00:50:04 +0200

    Client Application Started

---
+++ Project/Client-Application/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
